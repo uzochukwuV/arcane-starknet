108c4b39ec98e59a1c22d65282616ca3ec1bb7ee
uzochukwuv <vic.ezealor@gmail.com>
2025-01-14 16:44:05 +0100

    Initial commit with 🏗️ Scaffold-Stark 2

---
+++ packages/nextjs/services/web3/connectors.tsx
@@ -0,0 +1,38 @@
+import { argent, braavos, InjectedConnector } from "@starknet-react/core";
+import { getTargetNetworks } from "~~/utils/scaffold-stark";
+import { BurnerConnector } from "@scaffold-stark/stark-burner";
+import scaffoldConfig from "~~/scaffold.config";
+import { LAST_CONNECTED_TIME_LOCALSTORAGE_KEY } from "~~/utils/Constants";
+
+const targetNetworks = getTargetNetworks();
+
+export const connectors = getConnectors();
+
+// workaround helper function to properly disconnect with removing local storage (prevent autoconnect infinite loop)
+function withDisconnectWrapper(connector: InjectedConnector) {
+  const connectorDisconnect = connector.disconnect;
+  const _disconnect = (): Promise<void> => {
+    localStorage.removeItem("lastUsedConnector");
+    localStorage.removeItem(LAST_CONNECTED_TIME_LOCALSTORAGE_KEY);
+    return connectorDisconnect();
+  };
+  connector.disconnect = _disconnect.bind(connector);
+  return connector;
+}
+
+function getConnectors() {
+  const { targetNetworks } = scaffoldConfig;
+
+  const connectors: InjectedConnector[] = [argent(), braavos()];
+
+  if (
+    targetNetworks.some((network) => (network.network as string) === "devnet")
+  ) {
+    const burnerConnector = new BurnerConnector();
+    connectors.push(burnerConnector as unknown as InjectedConnector);
+  }
+
+  return connectors.sort(() => Math.random() - 0.5).map(withDisconnectWrapper);
+}
+
+export const appChains = targetNetworks;
---
+++ packages/nextjs/services/web3/provider.ts
@@ -0,0 +1,26 @@
+import scaffoldConfig from "~~/scaffold.config";
+import {
+  jsonRpcProvider,
+  publicProvider,
+  starknetChainId,
+} from "@starknet-react/core";
+import * as chains from "@starknet-react/chains";
+
+const containsDevnet = (networks: readonly chains.Chain[]) => {
+  return (
+    networks.filter((it) => it.network == chains.devnet.network).length > 0
+  );
+};
+
+const provider =
+  scaffoldConfig.rpcProviderUrl == "" ||
+  containsDevnet(scaffoldConfig.targetNetworks)
+    ? publicProvider()
+    : jsonRpcProvider({
+        rpc: () => ({
+          nodeUrl: scaffoldConfig.rpcProviderUrl,
+          chainId: starknetChainId(scaffoldConfig.targetNetworks[0].id),
+        }),
+      });
+
+export default provider;
